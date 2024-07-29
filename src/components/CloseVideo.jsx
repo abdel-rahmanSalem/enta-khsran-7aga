725cae3ace58e92b927c0976bd985d43bb0e9665
Abderlahman Salem <abdelrahmansalemfahmy@gmail.com>
2024-07-29 18:22:39 +0300

    fix: Prevent scrolling and better UX

--- src/components/CloseVideo.jsx
+++ src/components/CloseVideo.jsx
@@ -1,6 +1,6 @@
 function CloseVideo({ setIsOpenVideo }) {
   return (
-    <div className="fixed bottom-20 left-1/2 -translate-x-1/2">
+    <div className="fixed -bottom-32 left-1/2 -translate-x-1/2">
       <button
         className="flex h-10 w-10 items-center justify-center rounded-full bg-slate-700 text-xl text-gray-400 hover:bg-slate-700"
         onClick={() => setIsOpenVideo(false)}
--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -18,7 +18,7 @@ function MainHome() {
   }, [isOpenVideo]);
 
   return (
-    <div className="flex w-full justify-center px-6">
+    <div className="fixed left-1/2 flex w-full -translate-x-1/2 justify-center px-6">
       <div
         className={`w-full max-w-3xl rounded-lg bg-slate-800 ${isOpenVideo ? "px-6 py-7" : "px-8 py-6"} shadow-md`}
       >
