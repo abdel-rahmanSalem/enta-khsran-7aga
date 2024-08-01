970be63ef2487b25ab654f8eb3411cf88721b96c
Abderlahman Salem <abdelrahmansalemfahmy@gmail.com>
2024-08-01 17:39:37 +0300

    fix: CloseVideo btn

--- src/components/CloseVideo.jsx
+++ src/components/CloseVideo.jsx
@@ -1,6 +1,6 @@
 function CloseVideo({ setIsOpenVideo }) {
   return (
-    <div className="fixed -bottom-24 left-1/2 -translate-x-1/2">
+    <div className="">
       <button
         className="flex h-10 w-10 items-center justify-center rounded-full bg-slate-800 text-xl text-slate-500 hover:bg-slate-700"
         onClick={() => setIsOpenVideo(false)}
